452d6bc786678729aaf1d105ad25aab0b472d5e7
Rohit2968 <rohit2968raj@gmail.com>
2025-11-24 18:06:06 +0530

    PW Skills Web Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./dist/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
